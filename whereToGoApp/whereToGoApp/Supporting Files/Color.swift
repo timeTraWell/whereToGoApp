cd41326032ad9d5fe2ac066389cc5cffe7e001c4
Nickolay Nickitin <ndnickitin@gmail.com>
2019-03-03 20:19:34 +0300

    create fonts and color classes

---
+++ whereToGoApp/whereToGoApp/Supporting Files/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  whereToGoApp
+//
+//  Created by Nickolay Nickitin on 22/02/2019.
+//  Copyright © 2019 Nickolay Nickitin. All rights reserved.
+//
+
+import UIKit
+
+final class Color {
+
+    static let black = UIColor(red: 0, green: 0, blue: 0, alpha: 0.85)
+    static let gray = UIColor(red: 0, green: 0, blue: 0, alpha: 0.6)
+}
---
+++ whereToGoApp/whereToGoApp/Supporting Files/Fonts.swift
@@ -0,0 +1,16 @@
+//
+//  Fonts.swift
+//  whereToGoApp
+//
+//  Created by Nickolay Nickitin on 22/02/2019.
+//  Copyright © 2019 Nickolay Nickitin. All rights reserved.
+//
+
+import UIKit
+
+final class Fonts {
+    
+    static let titleFont = UIFont(name: "SFProDisplay-Medium", size: 30)
+    static let SFProText16 = UIFont(name: "SFProText-Regular", size: 16)
+    static let SFProText14 = UIFont(name: "SFProText-Regular", size: 14)
+}
