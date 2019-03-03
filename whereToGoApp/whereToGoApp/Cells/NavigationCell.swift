bed108319bb6041a61a891e4643d3f680206cec3
Nickolay Nickitin <ndnickitin@gmail.com>
2019-03-03 20:20:11 +0300

    create navigation cell

---
+++ whereToGoApp/whereToGoApp/Cells/NavigationCell.swift
@@ -0,0 +1,28 @@
+//
+//  NavigationCell.swift
+//  whereToGoApp
+//
+//  Created by Nickolay Nickitin on 25/02/2019.
+//  Copyright © 2019 Nickolay Nickitin. All rights reserved.
+//
+
+import UIKit
+
+class NavigationCell: UITableViewCell {
+
+    @IBOutlet weak var logoPic: UIImageView!
+    @IBOutlet weak var navButton: UIButton!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+    
+}
