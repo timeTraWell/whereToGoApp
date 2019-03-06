d3dfe3a32791c18be093ebbe17b7a3170fcd3055
Nickolay Nickitin <ndnickitin@gmail.com>
2019-03-06 21:27:59 +0300

    add event service for network

---
+++ whereToGoApp/whereToGoApp/EventsService.swift
@@ -0,0 +1,50 @@
+//
+//  EventsService.swift
+//  whereToGoApp
+//
+//  Created by Nickolay Nickitin on 04/03/2019.
+//  Copyright © 2019 Nickolay Nickitin. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+enum Result<ResultDataType> {
+    case data(ResultDataType)
+    case error(String)
+}
+
+
+class EventsService {
+    
+    func printInfo(_ value: Any) {
+        let t = type(of: value)
+        print("'\(value)' of type '\(t)'")
+    }
+    //id,dates,images,price
+    func loadEvents(eventsCount: String, completion: @escaping (Result<[Event]>) -> Void) {
+        let url = "https://kudago.com/public-api/v1.4/events/?fields=id,dates,images,price,title,description&page_size=\(eventsCount)&text_format=text&location=msk"
+        AF.request(url, method: .get).responseJSON { response in
+            guard response.result.isSuccess else {
+                return completion(.error("Ошибка при запросе данных \(String(describing: response.result.error))"))
+            }
+            
+            guard let json = response.result.value as? [String: Any] else {
+                return completion(.error("dataKek"))
+            }
+                
+            guard let resultsEvents = json["results"]  else {
+                return completion(.error("dataKek"))
+            }
+                
+            guard let data = try? JSONSerialization.data(withJSONObject: resultsEvents, options: JSONSerialization.WritingOptions.prettyPrinted) else {
+                return completion(.error("serializeKek"))
+            }
+                
+            guard let events = try? JSONDecoder().decode([Event].self, from: data) else {
+                return completion(.error("mappingKek"))
+            }
+            return completion(.data(events))
+        }
+    }
+}
