007b1555f663b4f8b87769b42cd4e5b22db8006f
Nickolay Nickitin <ndnickitin@gmail.com>
2019-03-16 01:03:14 +0300

    include outlets, init contents

--- whereToGoApp/whereToGoApp/ViewControllers/DetailEventViewController.swift
+++ whereToGoApp/whereToGoApp/ViewControllers/DetailEventViewController.swift
@@ -6,37 +6,126 @@
 //
 
 import UIKit
+import MapKit
 
 class DetailEventViewController: UIViewController {
     
     // MARK: - Properties
     
-    private var eventID: Int? = 0
-
+    private var event: Event?
+    
     // MARK: - IBOutlets
     
     @IBOutlet var container: UIView!
+    @IBOutlet weak var scrollView: UIScrollView!
     
+    // MARK: - Labels
+    @IBOutlet weak var eventHeaderLabel: UILabel!
+    @IBOutlet weak var eventDescriptionLabel: UILabel!
+    @IBOutlet weak var eventDetailLabel: UILabel!
+    @IBOutlet weak var eventGeoLabel: UILabel!
+    @IBOutlet weak var eventDateLabel: UILabel!
+    @IBOutlet weak var eventCostLabel: UILabel!
+    
+    @IBOutlet weak var geoIcon: UIImageView!
+    
+    @IBOutlet weak var buttonBack: UIButton!
+    @IBOutlet weak var map: MKMapView!
+    
+    // MARK: - IBActions
+    
+    @IBAction func buttonBackTouch(_ sender: Any) {
+        self.performSegue(withIdentifier: "backToMain", sender: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(self.eventID)
+        setupButtonBack()
+        setupContent()
+        
+//        print(self.eventID)
         // Do any additional setup after loading the view.
     }
     
-    public func setEventID(id: Int) {
-        self.eventID = id
+    public func setEvent(event: Event) {
+        self.event = event
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // MARK: - Private helpers
+    
+    private func setupContent() {
+        eventHeaderLabel.font = Fonts.SFProText20
+        eventHeaderLabel.textColor = Color.black
+        let eventName = castToString(data: event?.title)
+        eventHeaderLabel.text = eventName
+        
+        eventDescriptionLabel.font = Fonts.SFProTextSemibold
+        eventDescriptionLabel.textColor = Color.black
+        let eventDescription = castToString(data: event?.description)
+        eventDescriptionLabel.text = eventDescription
+        
+        eventDetailLabel.font = Fonts.SFProText16Reg
+        eventDetailLabel.textColor = Color.gray
+        let eventDetail = castToString(data: event?.body_text)
+        eventDetailLabel.text = eventDetail
+        
+        initSubLabels(subLabel: eventGeoLabel)
+        initSubLabels(subLabel: eventDateLabel)
+        initSubLabels(subLabel: eventCostLabel)
+        
+        let geo = castToString(data: event?.place?.address)
+        if geo != "" {
+            eventGeoLabel.text = geo
+        } else {
+            eventGeoLabel.isHidden = true
+            geoIcon.isHidden = true
+        }
+        
+        let price = castToString(data: event?.price)
+        if price == "" { // TODO: - perform ternar operatop
+            eventCostLabel.text = "бесплатно"
+        } else {
+            eventCostLabel.text = price
+        }
+        
+        guard let date = event?.dates?[0] else {
+            print("date cast error")
+            return
+        }
+        
+        let startDate = DateParser.getFormatedDate(intDate: date.start)
+        let endDate = DateParser.getFormatedDate(intDate: date.end)
+        
+        if (startDate != "error" && endDate != "error") {
+            eventDateLabel.text = startDate + " - " + endDate
+        } else {
+            print("start or end dataKekys")
+        }
     }
-    */
+    
+    private func castToString(data: String?) -> String {
+        guard let resultData = data else {
+            return ""
+        }
+        return resultData
+    }
+    
+    private func castToInt(value: Int?) -> Int {
+        guard let resultValue = value else {
+            return 0
+        }
+        return resultValue
+    }
+    
+    private func initSubLabels(subLabel: UILabel) {
+        subLabel.font = Fonts.getFont(fontName: "SFProText-Regular", size: 14)
+        subLabel.textColor = Color.gray
+    }
+    
+    private func setupButtonBack() {
+        buttonBack.backgroundColor = Color.white
+        buttonBack.layer.cornerRadius = 16
+    }
 
 }
