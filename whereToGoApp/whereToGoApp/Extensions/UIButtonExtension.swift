fc80aae70443b21c39bc4fbad0b98d0396037d09
Nickolay Nickitin <ndnickitin@gmail.com>
2019-03-18 23:57:42 +0300

    add extension for set spacing between text and image in uibutton

---
+++ whereToGoApp/whereToGoApp/Extensions/UIButtonExtension.swift
@@ -0,0 +1,18 @@
+//
+//  UIButtonExtension.swift
+//  whereToGoApp
+//
+//  Created by Nickolay Nickitin on 18/03/2019.
+//  Copyright © 2019 Nickolay Nickitin. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    func centerTextAndImage(spacing: CGFloat) {
+        let insetAmount = spacing / 2
+        imageEdgeInsets = UIEdgeInsets(top: 0, left: -insetAmount, bottom: 0, right: insetAmount)
+        titleEdgeInsets = UIEdgeInsets(top: 0, left: insetAmount, bottom: 0, right: -insetAmount)
+        contentEdgeInsets = UIEdgeInsets(top: 0, left: insetAmount, bottom: 0, right: insetAmount)
+    }
+}
