501814e353aa3a1418b5c82d0f1c6b113208d945
Nickolay Nickitin <ndnickitin@gmail.com>
2019-03-22 23:49:26 +0300

    add  collectionViewAdapter

---
+++ whereToGoApp/whereToGoApp/Adapter/ImageCollectionViewAdapter.swift
@@ -0,0 +1,9 @@
+//
+//  ImageCollectionViewAdapter.swift
+//  whereToGoApp
+//
+//  Created by Nickolay Nickitin on 22/03/2019.
+//  Copyright © 2019 Nickolay Nickitin. All rights reserved.
+//
+
+import Foundation
