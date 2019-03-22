3c1170b4e27f6eaa69e12180c5d6ebd0d172add6
Nickolay Nickitin <ndnickitin@gmail.com>
2019-03-23 01:18:07 +0300

    realize image pagination

--- whereToGoApp/whereToGoApp/Adapter/ImageCollectionViewAdapter.swift
+++ whereToGoApp/whereToGoApp/Adapter/ImageCollectionViewAdapter.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2019 Nickolay Nickitin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ImageCollectionViewAdapter: NSObject, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    private let cell = String(describing: ImageCollectionCell.self)
+    private var images: [ResponseImage]
+    
+    init(collectionView: UICollectionView, images: [ResponseImage]) {
+        collectionView.register(UINib(nibName: cell, bundle: .main), forCellWithReuseIdentifier: cell)
+        self.images = images
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let imageCollectionCell = collectionView.dequeueReusableCell(withReuseIdentifier: cell, for: indexPath) as? ImageCollectionCell else {
+            fatalError("imgcolKek")
+        }
+        imageCollectionCell.setImage(respondImageURL: self.images[indexPath.row].image)
+        return imageCollectionCell
+    }
+}
