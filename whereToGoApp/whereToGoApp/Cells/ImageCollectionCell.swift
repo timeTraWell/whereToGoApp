5a0761e50ab60aedf703b1074d54d975cad0951b
Nickolay Nickitin <ndnickitin@gmail.com>
2019-03-22 23:32:53 +0300

    add collectionViewCell to imageCollectionView

---
+++ whereToGoApp/whereToGoApp/Cells/ImageCollectionCell.swift
@@ -0,0 +1,18 @@
+//
+//  ImageCollectionCell.swift
+//  whereToGoApp
+//
+//  Created by Nickolay Nickitin on 22/03/2019.
+//  Copyright © 2019 Nickolay Nickitin. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
