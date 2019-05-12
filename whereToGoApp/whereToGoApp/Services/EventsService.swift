4aa0399cff06feccfb178b92e25729ee183a2fdb
Nickolay Nickitin <ndnickitin@gmail.com>
2019-05-12 21:02:41 +0300

    added additional wrapper for errors

--- whereToGoApp/whereToGoApp/Services/EventsService.swift
+++ whereToGoApp/whereToGoApp/Services/EventsService.swift
@@ -11,7 +11,30 @@ import Alamofire
 
 enum Result<ResultDataType> {
     case data(ResultDataType)
-    case error(String)
+    case error(ServiceError)
+}
+
+enum ServiceError: LocalizedError {
+    case resultsMissing
+    case dataMissing
+    case noResult
+    case serializeCrash
+    case mappingCrash
+    
+    var errorDescription: String? {
+        switch self {
+        case .resultsMissing:
+            return "result is missing"
+        case .noResult:
+            return "no results"
+        case .dataMissing:
+            return "data missing"
+        case .serializeCrash:
+            return "data serialization error"
+        case .mappingCrash:
+            return "data mapping error"
+        }
+    }
 }
 
 
@@ -21,23 +44,23 @@ class EventsService {
         let url = "https://kudago.com/public-api/v1.4/events/?fields=id,dates,place,images,price,title,description,body_text&expand=place&page_size=\(eventsCount)&text_format=text&location=msk"
         Alamofire.request(url, method: .get).responseJSON { response in
             guard response.result.isSuccess else {
-                return completion(.error("Ошибка при запросе данных \(String(describing: response.result.error))"))
+                return completion(.error(.noResult))
             }
             
             guard let json = response.result.value as? [String: Any] else {
-                return completion(.error("data response error"))
+                return completion(.error(.dataMissing))
             }
                 
             guard let resultsEvents = json["results"]  else {
-                return completion(.error("results in response missing"))
+                return completion(.error(.resultsMissing))
             }
                 
             guard let data = try? JSONSerialization.data(withJSONObject: resultsEvents, options: JSONSerialization.WritingOptions.prettyPrinted) else {
-                return completion(.error("serialize error"))
+                return completion(.error(.serializeCrash))
             }
                 
             guard let events = try? JSONDecoder().decode([Event].self, from: data) else {
-                return completion(.error("mapping error"))
+                return completion(.error(.mappingCrash))
             }
             return completion(.data(events))
         }
