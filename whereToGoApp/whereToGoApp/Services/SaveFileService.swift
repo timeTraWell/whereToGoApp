0e7f8696a02411134adf16c3e2dcd7e5d1a06d0e
Nickolay Nickitin <ndnickitin@gmail.com>
2019-09-04 23:41:04 +0300

    create SaveFile service

---
+++ whereToGoApp/whereToGoApp/Services/SaveFileService.swift
@@ -0,0 +1,13 @@
+//
+//  SaveFileService.swift
+//  whereToGoApp
+//
+//  Created by Nickolay Nickitin on 04/09/2019.
+//  Copyright © 2019 Nickolay Nickitin. All rights reserved.
+//
+
+import Foundation
+
+class SaveFileService {
+    //TODO:- perform logic to save file via user choose city
+}
