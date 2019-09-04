95339a1396fd2e3c3b0d742de83e62dde2f46021
Nickolay Nickitin <ndnickitin@gmail.com>
2019-09-04 22:30:11 +0300

    create City model

---
+++ whereToGoApp/whereToGoApp/Models/City.swift
@@ -0,0 +1,14 @@
+//
+//  City.swift
+//  whereToGoApp
+//
+//  Created by Nickolay Nickitin on 04/09/2019.
+//  Copyright © 2019 Nickolay Nickitin. All rights reserved.
+//
+
+import Foundation
+
+struct City: Codable {
+    let name: String
+    let slug: String
+}
