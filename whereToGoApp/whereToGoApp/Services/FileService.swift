0f2348a79bfc3df2c6d8f8fe6a4bc79ab67d32b1
Nickolay Nickitin <ndnickitin@gmail.com>
2019-09-05 16:24:37 +0300

    add func for save and load json file

--- whereToGoApp/whereToGoApp/Services/FileService.swift
+++ whereToGoApp/whereToGoApp/Services/FileService.swift
@@ -10,4 +10,54 @@ import Foundation
 
 class FileService {
     
+    //MARK:- Properties
+    private let fileName = "cityInfo.json"
+    
+    //MARK:- Public methods
+    public func saveToFile(city: City) -> Bool {
+        guard let data = try? JSONSerialization.data(withJSONObject: city.json, options: JSONSerialization.WritingOptions.prettyPrinted) else {
+            return false
+        }
+        
+        let path = getDocumentsDirectory().appendingPathComponent(fileName)
+        
+        do {
+            try data.write(to: path)
+        } catch {
+            return false
+        }
+        
+        return true
+    }
+    
+    public func loadFromFile() -> City? {
+        let path = getDocumentsDirectory().appendingPathComponent(fileName)
+        
+        do {
+            let fileContent = try String(contentsOf: path, encoding: .utf8)
+            
+            guard let data = fileContent.data(using: .utf8) else {
+                return nil
+            }
+            
+            guard let json = try JSONSerialization.jsonObject(with: data, options : .allowFragments) as? [String: Any] else {
+                return nil
+            }
+            
+            guard let city = City.parse(json: json) else {
+                return nil
+            }
+            
+            return city
+        }
+        catch {
+            return nil
+        }
+    }
+    
+    //MARK:- Private helper
+    private func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
 }
