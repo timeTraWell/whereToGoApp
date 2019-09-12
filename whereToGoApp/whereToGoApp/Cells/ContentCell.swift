bc4adb6230813f73a01e9c8600a71ed0ee285119
Nickolay Nickitin <ndnickitin@gmail.com>
2019-09-12 10:25:49 +0300

    index for debug help

--- whereToGoApp/whereToGoApp/Cells/ContentCell.swift
+++ whereToGoApp/whereToGoApp/Cells/ContentCell.swift
@@ -44,7 +44,7 @@ class ContentCell: UITableViewCell {
     }
 
     //MARK:- Setup func
-    func setupCell(event: Event) {
+    func setupCell(event: Event, index: Int) {
         eventNameLabel.text = event.title
         eventDescriptionLabel.text = event.description
         
